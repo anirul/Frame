d1ed9d40493692e500a4504e78720812c8f8b999
Frederic DUBOUCHET <frederic.dubouchet@gmail.com>
2020-03-30 16:10:39 +0200

    Corrected the camera

--- ShaderGLLib/Camera.cpp
+++ ShaderGLLib/Camera.cpp
@@ -1,13 +1,30 @@
 #include "Camera.h"
+#include <cmath>
+#if !defined(M_PI)
+#define M_PI   3.14159265358979323846264338327950288
+#endif
+#define DEG2RAD(angleInDegrees) ((angleInDegrees) * M_PI / 180.0)
+#define RAD2DEG(angleInRadians) ((angleInRadians) * 180.0 / M_PI)
 
 namespace sgl {
 
+	Camera::Camera(
+		const sgl::vector3& position /*= { 0.f, 0.f, 0.f }*/, 
+		const sgl::vector3& front /*= { 0.f, 0.f, -1.f }*/, 
+		const sgl::vector3& up /*= { 0.f, 1.f, 0.f }*/) :
+		position_(position),
+		front_(front),
+		up_(up)
+	{
+		UpdateCameraVectors();
+	}
+
 	const sgl::matrix Camera::LookAt() const
 	{
-		sgl::vector3 f = (to_ - pos_).Normalize();
-		sgl::vector3 u = (up_ - (f - up_)).Normalize();
-		sgl::vector3 s = (f % u).Normalize();
-		u = (s % f).Normalize();
+		sgl::vector3 f = (front_ - position_).Normalize();
+		sgl::vector3 u = up_;
+		sgl::vector3 s = (f % u.Normalize()).Normalize();
+		u = (s % f);
 
 		sgl::matrix4 result;
 		result(0, 0) = s.x;
@@ -16,23 +33,28 @@ namespace sgl {
 		result(0, 1) = u.x;
 		result(1, 1) = u.y;
 		result(2, 1) = u.z;
-		result(0, 2) = f.x;
-		result(1, 2) = f.y;
-		result(2, 2) = f.z;
-		result(3, 0) = pos_.x;
-		result(3, 1) = pos_.y;
-		result(3, 2) = pos_.z;
+		result(0, 2) = -f.x;
+		result(1, 2) = -f.y;
+		result(2, 2) = -f.z;
+		result(3, 0) = - (s * position_);
+		result(3, 1) = - (u * position_);
+		result(3, 2) = f * position_;
 		return result;
 	}
 
-	const sgl::vector3 Camera::Direction() const
+	void Camera::UpdateCameraVectors()
 	{
-		return (to_ - pos_).Normalize();
-	}
-
-	const sgl::vector3 Camera::Position() const
-	{
-		return pos_;
+		sgl::vector3 front;
+		front.x = 
+			static_cast<float>(
+				std::cos(DEG2RAD(yaw_)) * std::cos(DEG2RAD(pitch_)));
+		front.y = static_cast<float>(std::sin(DEG2RAD(pitch_)));
+		front.z = 
+			static_cast<float>(
+				std::sin(DEG2RAD(yaw_)) * std::cos(DEG2RAD(pitch_)));
+		front_ = front.Normalize();
+		right_ = (front_ % world_up_).Normalize();
+		up_ = (right_ % front_).Normalize();
 	}
 
 }	// End namespace sgl.
--- ShaderGLLib/Camera.h
+++ ShaderGLLib/Camera.h
@@ -7,18 +7,22 @@ namespace sgl {
 	class Camera {
 	public:
 		Camera(
-			const sgl::vector3& pos,
-			const sgl::vector3& to = { 0.f, 0.f, 1.f },
-			const sgl::vector3& up = { 0.f, 1.f, 0.f }) :
-			pos_(pos), to_(to), up_(up) {}
+			const sgl::vector3& position = { 0.f, 0.f, 0.f },
+			const sgl::vector3& front = { 0.f, 0.f, -1.f },
+			const sgl::vector3& up = { 0.f, 1.f, 0.f });
 		const sgl::matrix LookAt() const;
-		const sgl::vector3 Direction() const;
-		const sgl::vector3 Position() const;
+		const sgl::vector3 Front() const { return front_; }
+		const sgl::vector3 Position() const { return position_; }
 
 	protected:
-		sgl::vector3 pos_ = { 0, 0, 0 };
-		sgl::vector3 to_ = { 0, 0, 1 };
+		void UpdateCameraVectors();
+		sgl::vector3 position_ = { 0, 0, 0 };
+		sgl::vector3 front_ = { 0, 0, -1 };
 		sgl::vector3 up_ = { 0, 1, 0 };
+		sgl::vector3 right_ = { 1, 0, 0 };
+		sgl::vector3 world_up_ = { 0, 1, 0 };
+		float yaw_ = -90.0f;
+		float pitch_ = 0.0f;
 	};
 
 }	// End namespace sgl.
