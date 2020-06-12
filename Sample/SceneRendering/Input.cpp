66b5b69df5baf07db8f74c5a2266bbdbb76054d0
Frédéric Dubouchet <angel@calodox.org>
2020-06-12 09:55:07 +0200

    added movement
    Some bug with the env map

--- Sample/SceneRendering/Input.cpp
+++ Sample/SceneRendering/Input.cpp
@@ -37,7 +37,21 @@ void Input::MouseMoved(
 	const glm::vec2 position,
 	const glm::vec2 relative)
 {
-
+	const float inc = 0.01f;
+	auto camera = device_->GetCamera();
+	auto front = camera.GetFront();
+	auto right = camera.GetRight();
+	auto up = camera.GetUp();
+	if (relative.x) 
+	{
+		front += right * relative.x * inc;
+	}
+	if (relative.y)
+	{
+		front += up * relative.y * inc;
+	}
+	camera.SetFront(front);
+	device_->SetCamera(camera);
 }
 
 void Input::MousePressed(const char button)
