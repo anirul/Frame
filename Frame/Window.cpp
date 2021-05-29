d48c205de7a858f7bfc7408ecc1e98a52f7a1b21
Frédéric Dubouchet <angel@calodox.org>
2021-05-29 15:26:08 +0200

    Solved the problem with previous graphic card (changed OpenGL version to 4.5).

--- Frame/Window.cpp
+++ Frame/Window.cpp
@@ -269,7 +269,7 @@ namespace frame {
 				SDL_GL_CONTEXT_PROFILE_MASK, 
 				SDL_GL_CONTEXT_PROFILE_CORE);
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 #endif
 
 			// GL context.
