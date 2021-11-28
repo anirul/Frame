df8bceb64bdb58d10ee248fec46aff073160c7b4
Frédéric Dubouchet <angel@calodox.org>
2021-11-28 19:18:43 +0100

    Loading texture OTW

--- Frame/OpenGL/File/LoadTexture.cpp
+++ Frame/OpenGL/File/LoadTexture.cpp
@@ -5,8 +5,11 @@
 #include <set>
 #include "Frame/File/FileSystem.h"
 #include "Frame/File/Image.h"
+#include "Frame/OpenGL/Material.h"
 #include "Frame/OpenGL/Texture.h"
 #include "Frame/OpenGL/TextureCubeMap.h"
+#include "Frame/Proto/ParseLevel.h"
+#include "Frame/Proto/ProtoLevelCreate.h"
 
 namespace frame::opengl::file {
 
@@ -16,6 +19,94 @@ namespace frame::opengl::file {
 		std::set<std::string> basic_rgba_extention = { "png" };
 		std::set<std::string> cube_map_half_extention = { "hdr", "dds" };
 
+		proto::Level CreateEquirectangularProtoLevel()
+		{
+			proto::Level level{};
+			level.set_name("Equirectangular");
+			level.set_default_texture_name("OutputCubemap");
+			return level;
+		}
+
+		proto::ProgramFile CreateEquirectangularProtoProgramFile()
+		{
+			proto::ProgramFile program_file{};
+			proto::Program program{};
+			{
+				program.set_name("EquirectangularProgram");
+				program.set_shader("EquirectangularCubeMap");
+				proto::SceneType scene_type{};
+				scene_type.set_value(proto::SceneType::QUAD);
+				*program.mutable_input_scene_type() = scene_type;
+				program.add_output_texture_names("OutputCubemap");
+			}
+			*program_file.add_programs() = program;
+			return program_file;
+		}
+
+		proto::SceneTreeFile CreateEquirectangularProtoSceneTreeFile()
+		{
+			proto::SceneTreeFile scene_tree_file{};
+			{
+				proto::SceneMatrix scene_matrix{};
+				{
+					scene_matrix.set_name("root");
+				}
+				*scene_tree_file.add_scene_matrices() = scene_matrix;
+				proto::SceneCamera scene_camera{};
+				{
+					scene_camera.set_name("camera");
+					scene_camera.set_parent("root");
+					scene_camera.set_fov_degrees(90.0f);
+				}
+				*scene_tree_file.add_scene_cameras() = scene_camera;
+				scene_tree_file.set_default_root_name("root");
+				scene_tree_file.set_default_camera_name("camera");
+			}
+			return scene_tree_file;
+		}
+
+		proto::TextureFile CreateEquirectangularProtoTextureFile(
+			const std::string& input_file,
+			const std::pair<std::uint32_t, std::uint32_t> size,
+			const proto::PixelElementSize pixel_element_size,
+			const proto::PixelStructure pixel_structure)
+		{
+			proto::TextureFile texture_file{};
+			{
+				proto::Texture texture{};
+				texture.set_name("InputTexture");
+				texture.set_file_name(input_file);
+				*texture.mutable_pixel_element_size() = pixel_element_size;
+				*texture.mutable_pixel_structure() = pixel_structure;
+				*texture_file.add_textures() = texture;
+			}
+			{
+				proto::Texture texture{};
+				texture.set_name("OutputCubemap");
+				proto::Size proto_size{};
+				proto_size.set_x(size.first);
+				proto_size.set_y(size.second);
+				*texture.mutable_size() = proto_size;
+				*texture.mutable_pixel_element_size() = pixel_element_size;
+				*texture.mutable_pixel_structure() = pixel_structure;
+				*texture_file.add_textures() = texture;
+			}
+			return texture_file;
+		}
+
+		proto::MaterialFile CreateEquirectangularProtoMaterialFile()
+		{
+			proto::MaterialFile material_file{};
+			{
+				proto::Material material{};
+				material.set_name("EquirectangularMaterial");
+				material.set_program_name("EquirectangularProgram");
+				material.add_texture_names("InputTexture");
+				material.add_inner_names("Equirectangular");
+				*material_file.add_materials() = material;
+			}
+			return material_file;
+		}
 	}
 
 	std::optional<std::unique_ptr<frame::TextureInterface>> 
@@ -53,6 +144,21 @@ namespace frame::opengl::file {
 		const proto::PixelStructure pixel_structure 
 			/*= proto::PixelStructure_RGB()*/)
 	{
+		auto material = std::make_unique<frame::opengl::Material>();
+		auto frame = std::make_unique<frame::opengl::FrameBuffer>();
+		auto render = std::make_unique<frame::opengl::RenderBuffer>();
+		auto maybe_level = frame::proto::ParseLevelOpenGL(
+			{128, 128}, 
+			CreateEquirectangularProtoLevel(),
+			CreateEquirectangularProtoProgramFile(),
+			CreateEquirectangularProtoSceneTreeFile(),
+			CreateEquirectangularProtoTextureFile(),
+			CreateEquirectangularProtoMaterialFile());
+		ScopedBind scoped_bind_frame(*frame);
+		ScopedBind scoped_bind_render(*render);
+		auto equirectangular = 
+			LoadTextureFromFile(file, pixel_element_size, pixel_structure);
+		
 		throw std::runtime_error("Not implemented!");
 	}
 
