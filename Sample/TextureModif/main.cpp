0c0bf76ab4bec723d0570b45c6f961aa261d8c1a
Frédéric Dubouchet <angel@calodox.org>
2022-05-22 21:28:42 +0200

    changed the picture to compute the normals.

--- Sample/TextureModif/main.cpp
+++ Sample/TextureModif/main.cpp
@@ -26,7 +26,7 @@ int main(int ac, char** av)
 	{
 		Application app(
 			frame::file::FindFile("Asset/Json/TextureModif.json"),
-			frame::CreateSDLOpenGL({ 1280, 800 }));
+			frame::CreateSDLOpenGL({ 640, 512 }));
 		app.Startup();
 		app.Run();
 	}
