37ab08a0256259bed4ed6b4b254133437706deed
Frédéric Dubouchet <angel@calodox.org>
2025-06-19 11:00:54 +0200

    Unify cubemap view matrices

---
+++ src/frame/opengl/cubemap_views.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <array>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace frame::opengl
+{
+
+/**
+ * @brief View matrices for cubemap rendering.
+ */
+inline const std::array<glm::mat4, 6> kViewsCubemap = {
+    glm::lookAt(
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(-1.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f)),
+    glm::lookAt(
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(1.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f)),
+    glm::lookAt(
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, -1.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, 1.0f)),
+    glm::lookAt(
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, -1.0f)),
+    glm::lookAt(
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, 1.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f)),
+    glm::lookAt(
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, -1.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f))};
+
+/**
+ * @brief Projection matrix for cubemap rendering.
+ */
+inline const glm::mat4 kProjectionCubemap =
+    glm::perspective(glm::radians(90.0f), 1.0f, 0.01f, 10.0f);
+
+} // End namespace frame::opengl
