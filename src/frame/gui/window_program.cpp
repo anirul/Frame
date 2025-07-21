5327def825ddf2500f971413a0b56de821467d01
Frédéric Dubouchet <angel@calodox.org>
2025-07-21 16:30:09 +0200

    Fix initial node placement

--- src/frame/gui/window_program.cpp
+++ src/frame/gui/window_program.cpp
@@ -106,10 +106,12 @@ bool WindowProgram::DrawCallback()
     if (!initialized_)
     {
         ed::SetCurrentEditor(context_);
+        ed::Suspend();
         ed::SetNodePosition(entry_node, ImVec2(-250, 0));
         ed::SetNodePosition(program_node, ImVec2(0, 0));
         ed::SetNodePosition(exit_node, ImVec2(250, 0));
         ed::NavigateToContent();
+        ed::Resume();
         initialized_ = true;
     }
 
