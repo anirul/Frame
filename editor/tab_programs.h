43382be612346413b4a834f4bb28e1008062ce3c
Frédéric Dubouchet <angel@calodox.org>
2025-07-21 18:22:16 +0200

    Open program viewer in separate window

--- editor/tab_programs.h
+++ editor/tab_programs.h
@@ -3,7 +3,6 @@
 #include "tab_interface.h"
 #include "frame/gui/draw_gui_interface.h"
 #include "frame/program_interface.h"
-#include <imgui-node-editor/imgui_node_editor.h>
 
 namespace frame::gui {
 
@@ -11,15 +10,11 @@ class TabPrograms : public TabInterface {
   public:
     explicit TabPrograms(DrawGuiInterface& draw_gui)
         : TabInterface("Programs"), draw_gui_(draw_gui) {}
-    ~TabPrograms() override;
+    ~TabPrograms() override = default;
     void Draw(LevelInterface& level) override;
 
   private:
     DrawGuiInterface& draw_gui_;
-    ax::NodeEditor::Config config_{};
-    ax::NodeEditor::EditorContext* context_ = nullptr;
-    frame::ProgramInterface* current_program_ = nullptr;
-    bool initialized_ = false;
 };
 
 } // namespace frame::gui
