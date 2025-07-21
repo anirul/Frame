c26152801bffed4ab47384e2221898329b218f53
Frédéric Dubouchet <angel@calodox.org>
2025-07-21 17:32:19 +0200

    Display program node editor inline

--- editor/tab_programs.cpp
+++ editor/tab_programs.cpp
@@ -2,22 +2,155 @@
 #include "frame/entity_id.h"
 #include <imgui.h>
 
+namespace ed = ax::NodeEditor;
+
 namespace frame::gui {
 
-void TabPrograms::Draw(LevelInterface& level) {
-    if (ImGui::CollapsingHeader("Programs", ImGuiTreeNodeFlags_DefaultOpen)) {
-        for (auto id : level.GetPrograms()) {
-            auto& prog = level.GetProgramFromId(id);
-            if (ImGui::Selectable(prog.GetName().c_str())) {
-                draw_gui_.AddWindow(std::make_unique<WindowProgram>(level, prog));
-            }
-            if (ImGui::BeginDragDropSource()) {
-                EntityId payload = id;
-                ImGui::SetDragDropPayload("FRAME_ASSET_ID", &payload, sizeof(payload));
-                ImGui::Text("%s", prog.GetName().c_str());
-                ImGui::EndDragDropSource();
+TabPrograms::~TabPrograms()
+{
+    if (context_)
+    {
+        ed::DestroyEditor(context_);
+        context_ = nullptr;
+    }
+}
+
+void TabPrograms::Draw(LevelInterface& level)
+{
+    if (current_program_ == nullptr)
+    {
+        if (ImGui::CollapsingHeader("Programs", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            for (auto id : level.GetPrograms())
+            {
+                auto& prog = level.GetProgramFromId(id);
+                if (ImGui::Selectable(prog.GetName().c_str()))
+                {
+                    current_program_ = &prog;
+                    initialized_ = false;
+                    if (context_)
+                    {
+                        ed::DestroyEditor(context_);
+                        context_ = nullptr;
+                    }
+                }
+                if (ImGui::BeginDragDropSource())
+                {
+                    EntityId payload = id;
+                    ImGui::SetDragDropPayload("FRAME_ASSET_ID", &payload, sizeof(payload));
+                    ImGui::Text("%s", prog.GetName().c_str());
+                    ImGui::EndDragDropSource();
+                }
             }
         }
+    }
+    else
+    {
+        if (ImGui::Button("Back##program"))
+        {
+            current_program_ = nullptr;
+            initialized_ = false;
+            if (context_)
+            {
+                ed::DestroyEditor(context_);
+                context_ = nullptr;
+            }
+            return;
+        }
+
+        if (!context_)
+        {
+            config_.SettingsFile = nullptr;
+            context_ = ed::CreateEditor(&config_);
+        }
+
+        ed::SetCurrentEditor(context_);
+        ed::Begin("ProgramView");
+
+        int entry_node = 1;
+        int program_node = 2;
+        int exit_node = 3;
+
+        // Entry node with uniform outputs
+        ed::BeginNode(entry_node);
+        ImGui::Text("Entry");
+        int pin_index = 0;
+        for (const auto& uniform_name : current_program_->GetUniformNameList())
+        {
+            int pin_id = 10 + pin_index;
+            ed::BeginPin(pin_id, ed::PinKind::Output);
+            ImGui::Text("%s", uniform_name.c_str());
+            ed::EndPin();
+            ++pin_index;
+        }
+        ed::EndNode();
+
+        // Program node with uniform inputs and texture outputs
+        ed::BeginNode(program_node);
+        ImGui::Text("%s", current_program_->GetName().c_str());
+        pin_index = 0;
+        for (const auto& uniform_name : current_program_->GetUniformNameList())
+        {
+            int pin_id = 100 + pin_index;
+            ed::BeginPin(pin_id, ed::PinKind::Input);
+            ImGui::Text("%s", uniform_name.c_str());
+            ed::EndPin();
+            ++pin_index;
+        }
+        pin_index = 0;
+        for (auto tex_id : current_program_->GetOutputTextureIds())
+        {
+            int pin_id = 200 + pin_index;
+            ed::BeginPin(pin_id, ed::PinKind::Output);
+            auto& tex = level.GetTextureFromId(tex_id);
+            ImGui::Text("%s", tex.GetName().c_str());
+            ed::EndPin();
+            ++pin_index;
+        }
+        ed::EndNode();
+
+        // Exit node with texture inputs
+        ed::BeginNode(exit_node);
+        ImGui::Text("Exit");
+        pin_index = 0;
+        for (auto tex_id : current_program_->GetOutputTextureIds())
+        {
+            int pin_id = 300 + pin_index;
+            ed::BeginPin(pin_id, ed::PinKind::Input);
+            auto& tex = level.GetTextureFromId(tex_id);
+            ImGui::Text("%s", tex.GetName().c_str());
+            ed::EndPin();
+            ++pin_index;
+        }
+        ed::EndNode();
+
+        // Links from entry -> program
+        pin_index = 0;
+        for (const auto& uniform_name : current_program_->GetUniformNameList())
+        {
+            ed::Link(1000 + pin_index, 10 + pin_index, 100 + pin_index);
+            ++pin_index;
+        }
+
+        // Links from program -> exit
+        pin_index = 0;
+        for (auto tex_id : current_program_->GetOutputTextureIds())
+        {
+            ed::Link(2000 + pin_index, 200 + pin_index, 300 + pin_index);
+            ++pin_index;
+        }
+
+        if (!initialized_)
+        {
+            ed::SetNodePosition(entry_node, ImVec2(-250, 0));
+            ed::SetNodePosition(program_node, ImVec2(0, 0));
+            ed::SetNodePosition(exit_node, ImVec2(250, 0));
+            ed::NavigateToContent();
+            initialized_ = true;
+        }
+
+        ed::End();
+        ed::SetCurrentEditor(nullptr);
     }
 }
 
--- editor/tab_programs.h
+++ editor/tab_programs.h
@@ -2,7 +2,8 @@
 
 #include "tab_interface.h"
 #include "frame/gui/draw_gui_interface.h"
-#include "frame/gui/window_program.h"
+#include "frame/program_interface.h"
+#include <imgui-node-editor/imgui_node_editor.h>
 
 namespace frame::gui {
 
@@ -10,10 +11,15 @@ class TabPrograms : public TabInterface {
   public:
     explicit TabPrograms(DrawGuiInterface& draw_gui)
         : TabInterface("Programs"), draw_gui_(draw_gui) {}
+    ~TabPrograms() override;
     void Draw(LevelInterface& level) override;
 
   private:
     DrawGuiInterface& draw_gui_;
+    ax::NodeEditor::Config config_{};
+    ax::NodeEditor::EditorContext* context_ = nullptr;
+    frame::ProgramInterface* current_program_ = nullptr;
+    bool initialized_ = false;
 };
 
 } // namespace frame::gui
