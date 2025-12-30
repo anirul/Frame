1c8a27a118cf5a80a5c4ef51fc5e4191aab7693c
anirul <frederic.dubouchet@gmail.com>
2025-12-30 21:10:43 +0100

    Progress remove the include/ and also move project to relevant directory.

--- tests/frame/json/main.cpp
+++ tests/frame/json/main.cpp
@@ -5,14 +5,6 @@
 
 int main(int ac, char** av)
 {
-    if (std::getenv("SDL_VIDEODRIVER") == nullptr)
-    {
-#if defined(_WIN32)
-        _putenv_s("SDL_VIDEODRIVER", "dummy");
-#else
-        setenv("SDL_VIDEODRIVER", "dummy", 0);
-#endif
-    }
     testing::InitGoogleTest(&ac, av);
     return RUN_ALL_TESTS();
 }
