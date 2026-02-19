f55249c84313ba7036c1421c3047bebe61155ef6
anirul <frederic.dubouchet@gmail.com>
2026-02-19 10:31:39 +0100

    Fix ImNodeFlow path resolution for subproject builds

--- cmake/AddImNodeFlow.cmake
+++ cmake/AddImNodeFlow.cmake
@@ -1,13 +1,23 @@
+set(_frame_imnodeflow_dir "${CMAKE_CURRENT_LIST_DIR}/../external/ImNodeFlow")
+if(NOT EXISTS "${_frame_imnodeflow_dir}/src/ImNodeFlow.cpp")
+    set(_frame_imnodeflow_dir "${PROJECT_SOURCE_DIR}/external/ImNodeFlow")
+endif()
+if(NOT EXISTS "${_frame_imnodeflow_dir}/src/ImNodeFlow.cpp")
+    message(FATAL_ERROR "ImNodeFlow source not found. Checked ${CMAKE_CURRENT_LIST_DIR}/../external/ImNodeFlow and ${PROJECT_SOURCE_DIR}/external/ImNodeFlow.")
+endif()
+
 add_library(ImNodeFlow STATIC
-    ${CMAKE_SOURCE_DIR}/external/ImNodeFlow/src/ImNodeFlow.cpp
+    "${_frame_imnodeflow_dir}/src/ImNodeFlow.cpp"
 )
 
 target_include_directories(ImNodeFlow
-    PUBLIC ${CMAKE_SOURCE_DIR}/external/ImNodeFlow/include
+    PUBLIC "${_frame_imnodeflow_dir}/include"
 )
 
 target_compile_definitions(ImNodeFlow PRIVATE IMGUI_DEFINE_MATH_OPERATORS)
 
 find_package(imgui CONFIG REQUIRED)
 target_link_libraries(ImNodeFlow PRIVATE imgui::imgui)
+
+unset(_frame_imnodeflow_dir)
 
